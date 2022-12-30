a7bec9168a65d4345646a1f78e1532e37739bea8
nardleram <econosophy@gmail.com>
2022-12-30 12:39:57 +0000

    Apache error due to percentage character in nonce. Corrected

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,7 +11,7 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Elsie&display=swap&family=Quicksand:wght@300;400;500;600;700&display=swap" nonce="lKj8/6tGf$32_#hG+0hG=gByYwQ0&%">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}" nonce="lKj8/6tGf$32_#hG+0hG=gByYwQ0&%">
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}" nonce="lKj8/6tGf$32_#hG+0hG=gByYwQ0&$">
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
@@ -19,7 +19,7 @@
 
         <!-- Scripts -->
         @routes
-        <script src="{{ mix('js/app.js') }}" defer nonce="M65RtWhXKlQpO8&x!_dD#08UjYy+%"></script>
+        <script src="{{ mix('js/app.js') }}" defer nonce="M65RtWhXKlQpO8&x!_dD#08UjYy+$"></script>
     </head>
     <body class="font-sans antialiased bg-we4vBg">
         @inertia
--- resources/views/layouts/frame.blade.php
+++ resources/views/layouts/frame.blade.php
@@ -8,10 +8,10 @@
         <title>{{ config('app.name', 'we4v') }}</title>
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Quicksand:wght@200;300;400;500;600;700&display=swap" nonce="lKj8/6tGf$32_#hG+0hG=gByYwQ0&%">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Quicksand:wght@200;300;400;500;600;700&display=swap" nonce="lKj8/6tGf$32_#hG+0hG=gByYwQ0&&">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}" nonce="lKj8/6tGf$32_#hG+0hG=gByYwQ0&%">
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}" nonce="lKj8/6tGf$32_#hG+0hG=gByYwQ0&$">
 
         <!-- Scripts -->
         @routes
